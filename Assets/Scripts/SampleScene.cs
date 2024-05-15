bbd1e84b5c4d003104ea761d62b392cb607354f1
CANDRGROUP\hiroe.mochizuki <hiroe.mochizuki@claytechworks.jp>
2024-05-15 12:48:26 +0900

    update : メッセージ変更

--- Assets/Scripts/SampleScene.cs
+++ Assets/Scripts/SampleScene.cs
@@ -6,7 +6,7 @@ public class SampleScene : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var hoge = "test hoge";
+        var hoge = "test hoge message";
         Debug.Log($"{hoge}");
     }
 
